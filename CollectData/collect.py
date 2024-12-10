99848c0e3fb3d48fe9161f8b92eb4bea6a8270b2
unknown <ashish.kumar.singh.jee@gmail.com>
2024-12-10 12:29:29 +0530

    #Driver-Set-Up

--- CollectData/collect.py
+++ CollectData/collect.py
@@ -1,21 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
 
 PATH="chromedriver.exe"
+URL="https://www.naukri.com/data-analyst-jobs"
+search_bar_class="nI-gNb-search-bar"
+#options 
 
-
+option=Options()
+option.headless=True
 
 
 service=Service(executable_path=PATH)
-driver=webdriver.Chrome(service=service)
+driver=webdriver.Chrome(options=option,service=service)
 
 try:
-    driver.get("https://www.github.com/ashdude14/DA4")
+    driver.get(URL)
+    
 except Exception as e:
     print(e)
 finally:
-    print(driver.title)
+    time.sleep(10)
+    filter_class=driver.find_element(By.CLASS_NAME,search_bar_class)
+    #filter_class.click()
+    print(filter_class)
     driver.quit()
 
    
