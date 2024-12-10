4f977fa9a0e927ad1172f8ad24a445bbad9eb1ad
unknown <ashish.kumar.singh.jee@gmail.com>
2024-12-10 19:39:25 +0530

    #Desc-of-web-scraping

--- CollectData/collect.py
+++ CollectData/collect.py
@@ -6,11 +6,13 @@ import time
 
 PATH="chromedriver.exe"
 URL="https://www.naukri.com/data-analyst-jobs"
-search_bar_class="nI-gNb-search-bar"
+filter_class="styles_filterCount__Xn_PG"
+filter_par="styles_ellipsis__cvWP1 styles_filterLabel__jRP04"
+cont_list="cust-job-tuple layout-wrapper lay-2 sjw__tuple"
 #options 
 
 option=Options()
-option.headless=True
+#option.add_argument("--headless=new")
 
 
 service=Service(executable_path=PATH)
@@ -23,9 +25,13 @@ except Exception as e:
     print(e)
 finally:
     time.sleep(10)
-    filter_class=driver.find_element(By.CLASS_NAME,search_bar_class)
+    filter_class=driver.find_elements(By.CSS_SELECTOR,".row1, .row2,.row3, .row4, .row5, .row6") 
     #filter_class.click()
-    print(filter_class)
+    count=0
+    for element in filter_class : 
+        print(element.text)
+        count+=1
+    print('total counts= ',count)
     driver.quit()
 
    
